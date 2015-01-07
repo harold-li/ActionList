f2715143a6f4a232da358bf2a72817718d222070
power <power0721@gmail.com>
2015-01-07 22:54:20 +0800

    add dataSource

--- src/main/java/org/power/action/ActionListApplication.java
+++ src/main/java/org/power/action/ActionListApplication.java
@@ -1,9 +1,13 @@
 package org.power.action;
 
+import javax.sql.DataSource;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 
 @Configuration
 @ComponentScan
@@ -12,5 +16,16 @@ public class ActionListApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ActionListApplication.class, args);
+	}
+
+	@Bean
+	public DataSource dataSource() {
+		SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+		dataSource.setDriverClass(org.h2.Driver.class);
+		dataSource.setUsername("sa");
+		dataSource.setUrl("jdbc:h2:mem");
+		dataSource.setPassword("");
+
+		return dataSource;
 	}
 }
