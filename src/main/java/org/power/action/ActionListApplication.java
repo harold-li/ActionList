d93cf4cd6beea5f6b150743392f70b99dab31599
power <power0721@gmail.com>
2015-01-10 19:33:14 +0800

    add data source for jae

--- src/main/java/org/power/action/ActionListApplication.java
+++ src/main/java/org/power/action/ActionListApplication.java
@@ -38,14 +38,57 @@ public class ActionListApplication {
    */
   @Bean(name = "dataSource", destroyMethod = "close")
   @Profile("mopass")
-  public DataSource dataSource() {
-    String dbUrl = "jdbc:mysql://10.4.26.93:3306/d1bf3727d27474fc39b161ac1c8ff1e0f?useUnicode=true&characterEncoding=UTF-8";
+  public DataSource oscDataSource() {
+    String dbUrl =
+        "jdbc:mysql://10.4.26.93:3306/" + getProperty("db_name") + "?useUnicode=true&characterEncoding=UTF-8";
     DruidDataSource dataSource = new DruidDataSource();
 
     // Basic attribute
     dataSource.setUrl(dbUrl);
     dataSource.setUsername(getProperty("db_username"));
     dataSource.setPassword(getProperty("db_password"));
+
+    // Pool size
+    dataSource.setInitialSize(8);
+    dataSource.setMinIdle(8);
+    dataSource.setMaxActive(128);
+
+    // Connection wait time limit
+    dataSource.setMaxWait(DateUtils.MILLIS_PER_MINUTE);
+
+    // Time between connection check
+    dataSource.setTimeBetweenEvictionRunsMillis(DateUtils.MILLIS_PER_MINUTE);
+
+    // Time of connection max live time
+    dataSource.setMinEvictableIdleTimeMillis(5 * DateUtils.MILLIS_PER_MINUTE);
+
+    // Connection test
+    dataSource.setValidationQuery("SELECT 'x'");
+    dataSource.setTestWhileIdle(true);
+    dataSource.setTestOnBorrow(false);
+    dataSource.setTestOnReturn(false);
+
+    // PS cache
+    dataSource.setPoolPreparedStatements(true);
+    dataSource.setMaxPoolPreparedStatementPerConnectionSize(20);
+
+    return dataSource;
+  }
+
+  /**
+   * Bean: Data source
+   *
+   * @return dataSource bean
+   */
+  @Bean(name = "dataSource", destroyMethod = "close")
+  @Profile("jae")
+  public DataSource jaeDataSource() {
+    DruidDataSource dataSource = new DruidDataSource();
+
+    // Basic attribute
+    dataSource.setUrl(getProperty("db.url"));
+    dataSource.setUsername(getProperty("db.username"));
+    dataSource.setPassword(getProperty("db.password"));
 
     // Pool size
     dataSource.setInitialSize(8);
